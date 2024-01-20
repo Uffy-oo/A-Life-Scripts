481e62c3551020560d2f010d38b8263450d89183
Uffy-oo <yufei.li2@mail.mcgill.ca>
2024-01-21 04:51:50 +0800

    GameManager

---
+++ GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    private int currentYear = 1;
+    private int currentSemester = 1;
+    private void Awake()
+
+    public Text GPA;
+
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
